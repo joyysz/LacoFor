746309b264c1c666d93211abb9734a029b29705e
Joyce <joycesferreira12345@gmail.com>
2026-05-14 15:08:26 -0300

    Adiciona saldo dinâmico e desativa botão

--- Simulador.Tigrinho/MainWindow.xaml.cs
+++ Simulador.Tigrinho/MainWindow.xaml.cs
@@ -1,41 +1,37 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Simulador.Tigrinho;
 
 public partial class MainWindow : Window
 {
+    public decimal saldoInicial = 10M;
+
     public MainWindow()
     {
         InitializeComponent();
     }
-    
-    private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
-        {
-            var quantidadeTexto = tbQuantidade.Text;
-            var quantidadeSorteios = Convert.ToInt32(quantidadeTexto);
-            //var quantidadeSorteios = Convert.ToInt32(tbQuantidade.Text);
 
-            if (quantidadeSorteios < 1)
-            {
-                quantidadeSorteios = 1;
-            }
-        
-            var sorteador = new Random();
-            // contador++ ; contador += 1; contador = contador + 1
-            for (int contador = 0; contador < quantidadeSorteios; contador++)
+    private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
+    {
+        btnSorteio.IsEnabled = false;
+
+        var quantidadeTexto = tbQuantidade.Text;
+        var quantidadeSorteios = Convert.ToInt32(quantidadeTexto);
+        if (quantidadeSorteios < 1) quantidadeSorteios = 1;
+
+        var sorteador = new Random();
+        for (var contador = 0; contador < quantidadeSorteios; contador++)
+            if (saldoInicial >= 10)
             {
                 tbResultado.Text = sorteador.Next(0, 40000000).ToString();
                 await Task.Delay(1000);
             }
-        
-        }
+            else
+            {
+                MessageBox.Show("Você não tem saldo suficiente para realizar o sorteio!");
+                break;
+            }
+
+        btnSorteio.IsEnabled = true;
     }
+}
