24be5e7656dcea2c72a7f30c344b54458e967229
Joyce <joycesferreira12345@gmail.com>
2026-05-14 16:35:14 -0300

    Acrescenta estruturas de testes

--- Simulador.Tigrinho/MainWindow.xaml.cs
+++ Simulador.Tigrinho/MainWindow.xaml.cs
@@ -9,21 +9,41 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        tbSaldo.Text = $"R$ {saldoInicial}";
     }
 
     private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
     {
-        btnSorteio.IsEnabled = false;
 
         var quantidadeTexto = tbQuantidade.Text;
-        var quantidadeSorteios = Convert.ToInt32(quantidadeTexto);
+        int quantidadeSorteios;
+ 
+        try
+        {
+            quantidadeSorteios = Convert.ToInt32(quantidadeTexto);
+        }
+        catch (FormatException)
+        {
+            MessageBox.Show("Entrada válida. Coloque apenas números de 1 para cima!");
+            return;
+        }
+        catch (OverflowException)
+        {
+            MessageBox.Show("Erro! O número digitado é maior que o número suportado!");
+            return;
+        }
+        
+        btnSorteio.IsEnabled = false;
         if (quantidadeSorteios < 1) quantidadeSorteios = 1;
 
         var sorteador = new Random();
         for (var contador = 0; contador < quantidadeSorteios; contador++)
+        {
             if (saldoInicial >= 10)
             {
                 tbResultado.Text = sorteador.Next(0, 40000000).ToString();
+                saldoInicial -= 10;
+                tbSaldo.Text = $"R$ {saldoInicial}";
                 await Task.Delay(1000);
             }
             else
@@ -31,7 +51,7 @@ public partial class MainWindow : Window
                 MessageBox.Show("Você não tem saldo suficiente para realizar o sorteio!");
                 break;
             }
-
+        }
         btnSorteio.IsEnabled = true;
     }
 }
